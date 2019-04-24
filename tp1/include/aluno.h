ac74ea4eb7c9a4b338e28c402611d284cee4fd4b
Abner de Marcos Neves <abnerneves@jordao.grad.dcc.ufmg.br>
2019-04-24 19:53:40 -0300

    arquivos e pastas criados, aluno.h e curso.h iniciados

---
+++ tp1/include/aluno.h
@@ -0,0 +1,17 @@
+#ifndef ALUNO_H
+#define ALUNO_H
+
+#include <string>
+
+class Aluno {
+private:
+    std::string nome;
+    unsigned int nota;
+    unsigned int p; //primeira opcao
+    unsigned int s; //segunda opcao
+    
+public:
+    
+};
+
+#endif
---
+++ tp1/include/curso.h
@@ -0,0 +1,17 @@
+#ifndef CURSO_H
+#define CURSO_H
+
+#include <string>
+
+class Curso {
+private:
+    string nome;
+    unsigned int vagas;
+    Classificados classificados;
+    Espera espera;
+    
+public:
+
+};
+
+#endif
