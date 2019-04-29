19a2b2be82d92cf4cc4e83c2989818e69e43864b
Ábner Neves <abnermneves@gmail.com>
2019-04-29 20:57:48 +0000

    ids implementados

--- tp1/program/main.cpp
+++ tp1/program/main.cpp
@@ -4,11 +4,11 @@
 
 int main () {
   Lista l = Lista();
-  Aluno a1 = Aluno("Samantha!", 700.51, 3, 2);
-  Aluno a2 = Aluno("Percoço", 580.04, 1, 4);
-  Aluno a3 = Aluno("Zé Pikeno", 235.67, 3, 2);
-  Aluno a4 = Aluno("Wanda", 499.84, 1, 2);
-  Aluno a5 = Aluno("Séverine", 600.54, 4, 3);
+  Aluno a1 = Aluno(1, "Samantha!", 700.51, 3, 2);
+  Aluno a2 = Aluno(2, "Percoço", 580.04, 1, 4);
+  Aluno a3 = Aluno(3, "Zé Pikeno", 235.67, 3, 2);
+  Aluno a4 = Aluno(4, "Wanda", 499.84, 1, 2);
+  Aluno a5 = Aluno(5, "Séverine", 600.54, 4, 3);
 
   l.inserir(&a1);
   l.inserir(&a2);
