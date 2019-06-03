f38553d3703abf92bb8c51f345a80ebb38769440
Abner de Marcos Neves <abnerneves@araguaia.grad.dcc.ufmg.br>
2019-06-03 20:10:03 -0300

    main iniciado e alguns arquivos criados

--- tp2/src/main.cpp
+++ tp2/src/main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <string>
 #include "teste.cpp"
+using namespace std;
 
 int main(int argc, char* argv[]){
   teste(argc, argv);
+  string varQS = argv[1];
+  string tipoVetor = argv[2];
+  unsigned int tam = stoi(argv[3]);
+  bool exibir = false;
+  
+  if (argc == 5){
+    string p = argv[4];
+    if(p == "-p")
+      exibir = true;
+  }
+  
+  int vetor[tam];
+  
+  if(tipoVetor == "Ale"){
+  
+  } else if (tipoVetor == "OrdC"){
+  
+  } else if (tipoVetor == "OrdD"){
+  
+  } else {
+    cout << "Tipo de vetor inválido!" << endl;
+  }
+  
+  if(varQS == "QC"){
+      cout << "QC" << endl;
+  } else if(varQS == "QM3"){
+      cout << "QM3" << endl;
+  } else if(varQS == "QPE"){
+      cout << "QPE" << endl;
+  } else if(varQS == "QI1"){
+    
+  } else if(varQS == "QI5"){
+    
+  } else if(varQS == "QI10"){
+    
+  } else if(varQS == "QNR"){
+  
+  } else{
+      cout << "QuickSort inválido!" << endl;  
+  }
+  
   return 0;
 }
